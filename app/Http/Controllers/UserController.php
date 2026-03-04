f387d8ffec953a4bfed738acff468dc76308ee85
Bima Adiwijaya <bimaadiwijaya83@gmail.com>
2026-03-04 16:29:34 +0700

    Jobsheet3_Praktikum6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserModel; // Pastikan untuk mengimpor model UserModel
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function profile($id, $name)
     {
         return view('user.profile', compact('id', 'name'));
+    }
+
+    public function index()
+    {
+        // tambah data user dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        
+        // update data user di mana username adalah customer-1
+        UserModel::where('username', 'customer-1')->update($data);
+
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
     }
 }
