1d19bb071a5d3b66f41dbc0c0012d91bf763c9ba
Bima Adiwijaya <bimaadiwijaya83@gmail.com>
2026-03-09 02:46:34 +0700

    Jobsheet4_Praktikum2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,8 +15,17 @@ class UserController extends Controller
 
     public function index()
     {
-        $user = UserModel::where('level_id', 2)->count();
-        // dd($user);
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama'     => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+
+        $user->save(); // Perubahan utama ada di baris ini
+
         return view('user', ['data' => $user]);
     }
 }
