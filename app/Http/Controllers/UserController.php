07e55662fcc0db0d4aa5dcfc76b0686f106d849c
Bima Adiwijaya <bimaadiwijaya83@gmail.com>
2026-03-13 05:17:22 +0700

    Jobsheet4_Praktikum2.6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,27 +15,52 @@ class UserController extends Controller
 
     public function index()
     {
-        // Membuat user baru di database
-        $user = UserModel::create([
-            'username' => 'manager11',
-            'nama'     => 'Manager11',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama'     => $request->nama,
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id
         ]);
 
-        // Mengubah nilai atribut username pada objek $user
-        $user->username = 'manager12';
+        return redirect('/user');
+    }
 
-        // Menyimpan perubahan ke database
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make('$request->password');
+        $user->level_id = $request->level_id;
+
         $user->save();
 
-        // Mengecek status perubahan menggunakan wasChanged()
-        $user->wasChanged(); // true
-        $user->wasChanged('username'); // true
-        $user->wasChanged(['username', 'level_id']); // true
-        $user->wasChanged('nama'); // false
-        
-        // Melakukan dump and die untuk hasil pengecekan array atribut
-        dd($user->wasChanged(['nama', 'username'])); // true
+        return redirect('/user');
+    }
+
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
     }
 }
